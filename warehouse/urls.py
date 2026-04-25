6443880d04a2117ae5a5acfbf677563fd1ebde0e
RomahaPoleha <romashkoo2023@list.ru>
2026-04-26 02:54:58 +0300

    создал docer

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -19,7 +19,10 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import HttpResponse
+
 urlpatterns = [
+    path('test-ok/', lambda r: HttpResponse("✅ WORKS!")),
     path('admin/', admin.site.urls),
     path('', include('accounting.urls')),
 ]
