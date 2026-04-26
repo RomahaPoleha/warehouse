e1d63151a1d0f0f533029787ed71d6062aedb8c6
RomahaPoleha <romashkoo2023@list.ru>
2026-04-26 05:02:33 +0300

    изменение настроек

--- warehouse/urls.py
+++ warehouse/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.http import HttpResponse
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
